ed58abf4a4577c42e1cf7238fa6e82cc59ea51d5
Januar Asprilla <ajanuarandres@gmail.com>
2024-02-23 11:12:01 -0500

    fix: corrections Vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,13 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "styled-components"],
+      external: [
+        "react",
+        "styled-components",
+        "react/jsx-runtime",
+        "react-dom",
+        "@inubekit/foundations",
+      ],
       output: {
         globals: {
           react: "React",
