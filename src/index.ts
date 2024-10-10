f817567249065e11994ba30ff9d9a26da50db9d1
Cesar Marin Alfonso <45011420+cmarin001@users.noreply.github.com>
2024-10-10 12:35:29 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Label } from "./Label";
 export type { ILabel } from "./Label";
 export type { ILabelSize } from "./Label/props";
+export { tokens as LabelTokens } from "./Label/Tokens/tokens";
